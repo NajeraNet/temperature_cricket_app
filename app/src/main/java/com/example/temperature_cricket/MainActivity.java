0462812775ffbc08c66e7b1a0c401323cc5c3c95
NajeraNet <najeranet.com@gmail.com>
2023-02-27 19:15:18 -0600

    Se establece la pantalla que usaremos para comensar la programacion logica

--- app/src/main/java/com/example/temperature_cricket/MainActivity.java
+++ app/src/main/java/com/example/temperature_cricket/MainActivity.java
@@ -1,17 +1,19 @@
 package com.example.temperature_cricket;
 
 import android.os.Bundle;
-
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+//  Se declaran las variables
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 //      Here is where the magic start
+        //
 
     }
 }
