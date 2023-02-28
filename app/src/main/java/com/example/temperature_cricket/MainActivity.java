805207b9bb9219fd2e3ef200ceb727c4dc5acd61
NajeraNet <najeranet.com@gmail.com>
2023-02-27 21:56:16 -0600

    Se reformo MainActivity agregando una condicion de que se deben llenar todos los campos antes de pedir el resultado. tambien se agrego una variable de tipo DecimalFormat, nombrada "formatter" para darle una mejor vista al resultado al momento de pasarlo al textview(formatter.format(temp))

--- app/src/main/java/com/example/temperature_cricket/MainActivity.java
+++ app/src/main/java/com/example/temperature_cricket/MainActivity.java
@@ -5,8 +5,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     EditText etChirps;
     Button btnSubmit;
     TextView tvResults;
+    DecimalFormat formatter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,16 +28,31 @@ public class MainActivity extends AppCompatActivity {
         etChirps = findViewById(R.id.etChirps);
         btnSubmit = findViewById(R.id.btnSubmit);
         tvResults = findViewById(R.id.tvResults);
-
+//
 //      Removemos la vista al iniciar
         tvResults.setVisibility(View.GONE);
-
+//
+        formatter = new DecimalFormat("#0.0");
+//
         btnSubmit.setOnClickListener(new View.OnClickListener(){
 
             @Override
             public void onClick(View TempOut) {
-
-//              Obtenemos el numero de chirridos
+//              Version corregida y aumentada ;)
+//              Revisamos si el usuario no ha escrito algo en el campo de texto
+                if (etChirps.getText().toString().isEmpty()) {
+                    Toast.makeText(MainActivity.this, "¡Llena todos los campos!", Toast.LENGTH_SHORT).show();
+                } else {
+//                  Obtenemos y convertimos el numero de chirridos de una y lo almacenamos en la variable entera chirps
+                    int chirps = Integer.parseInt(etChirps.getText().toString().trim());
+//                  Calculamos la temperatura y la agregamos a la variable temp
+                    double temp = (chirps / 3.0) + 4;
+//                  Pasamos los resultados a unn String
+                    String results = "La temperatura aproximada es de " + formatter.format(temp) + " grados celcius";
+//                  mostramos los resultados
+                    tvResults.setText(results);
+                }
+/*              Obtenemos el numero de chirridos
                 int nChirps = Integer.parseInt((etChirps.getText().toString()));
 
 //              Los dividimos entre 3 y le sumamos 4
@@ -46,6 +65,8 @@ public class MainActivity extends AppCompatActivity {
                 tvResults.setText(textview);
 //
 //              Traemos la vista de regreso para mostrar los resultados
+
+ */
                 tvResults.setVisibility(View.VISIBLE);
             }
         });
