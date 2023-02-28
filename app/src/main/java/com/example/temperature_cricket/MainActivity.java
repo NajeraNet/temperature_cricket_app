04b14457c49405552c16374bfc3a9b1b599c5bad
NajeraNet <najeranet.com@gmail.com>
2023-02-27 20:18:14 -0600

    Se declararon las variables, sea asignaron para modificarlas y obtener valores, se obtuvo el numero de chirridos y se convirtio a entero, se calculo con la formula proporcionada, se agrego la descripcion del resultado y se paso todo a una cadena de texto, la cadena de texto se paso al text viw y se puso como visible. y al final no la cajeteamos haha. que chido

--- app/src/main/java/com/example/temperature_cricket/MainActivity.java
+++ app/src/main/java/com/example/temperature_cricket/MainActivity.java
@@ -1,19 +1,53 @@
 package com.example.temperature_cricket;
 
 import android.os.Bundle;
-import androidx.annotation.Nullable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
 //  Se declaran las variables
+    EditText etChirps;
+    Button btnSubmit;
+    TextView tvResults;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 //      Here is where the magic start
-        //
+        //Asignamos a variables
+        etChirps = findViewById(R.id.etChirps);
+        btnSubmit = findViewById(R.id.btnSubmit);
+        tvResults = findViewById(R.id.tvResults);
 
+//      Removemos la vista al iniciar
+        tvResults.setVisibility(View.GONE);
+
+        btnSubmit.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View TempOut) {
+
+//              Obtenemos el numero de chirridos
+                int nChirps = Integer.parseInt((etChirps.getText().toString()));
+
+//              Los dividimos entre 3 y le sumamos 4
+                int result = nChirps / 3 + 4;
+
+//              Los convertimos como string
+                String textview = getString(R.string.tvDescFin) + " " + String.valueOf(result);
+
+//              Los pasamos a tvResult
+                tvResults.setText(textview);
+//
+//              Traemos la vista de regreso para mostrar los resultados
+                tvResults.setVisibility(View.VISIBLE);
+            }
+        });
     }
 }
